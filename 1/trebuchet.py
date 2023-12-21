070d3684cd9abfd0e0b64cdeebe46ec75eab77cf
Burak Aktas <aburakaktas@gmail.com>
2023-12-21 16:50:50 +0100

    solved second half of week 1, much speed wow

--- 1/trebuchet.py
+++ 1/trebuchet.py
@@ -9,11 +9,26 @@ def find_last_digit(raw_key: str):
         if char.isnumeric():
             return char
 
+change = {
+    "one" : "one1one",
+    "two" : "two2two",
+    "three" : "three3three",
+    "four" : "four4four",
+    "five" : "five5five",
+    "six" : "six6six",
+    "seven" : "seven7seven",
+    "eight" : "eight8eight",
+    "nine" : "nine9nine"
+}
+
+
 def main():
     sum = 0
     with open("data.txt") as file:
         for line in file:
             raw_key = line.strip()
+            for key, value in change.items():
+                raw_key = raw_key.replace(key, value)
             first_digit = find_first_digit(raw_key)
             last_digit = find_last_digit(raw_key)
             code = first_digit + last_digit
